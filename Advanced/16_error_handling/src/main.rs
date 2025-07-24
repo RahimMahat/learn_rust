d35184bc281627a06839d0c9f37181f2a357d4f9
RahimMahat <rahimmahat07@gmail.com>
2025-07-24 10:30:14 +0530

    error propagating notes

--- Advanced/16_error_handling/src/main.rs
+++ Advanced/16_error_handling/src/main.rs
@@ -1,37 +1,42 @@
 use std::fs::File;
-use std::io::{Read, stdin};
-use std::process;
+use std::io::{self, Read, stdin};
 
-fn file_handling() {
+fn file_handling() -> Result<String, io::Error> {
     // Opening a file.
     println!("Enter filename to open: ");
     let mut input = String::new();
     let user_requested_file = stdin().read_line(&mut input);
 
     if let Err(err) = user_requested_file {
-        eprintln!("Something went wrong getting user input: Error: {err}");
-        process::exit(1)
+        return Err(err);
     }
 
     let mut file = match File::open(input.trim()) {
         Ok(file) => file,
         Err(err) => {
             // Prints to the standard error, while println! prints to the standard output, with a new line.
-            eprintln!("Something went wrong opening the file, error: {err}");
+            // eprintln!("Something went wrong opening the file, error: {err}");
             // exit the program gracefully with an error code without panicking.
-            process::exit(1);
+            // process::exit(1);
+            return Err(err);
         }
     };
 
     let mut file_contents = String::new();
     let read_operation = file.read_to_string(&mut file_contents);
     if let Err(err) = read_operation {
-        eprintln!("Something went wrong reading the file: error: {err}");
-        process::exit(1)
+        return Err(err);
     }
-    println!("{file_contents}");
+
+    Ok(file_contents)
 }
 
 fn main() {
-    file_handling();
+    let file_result = file_handling();
+    match file_result {
+        Ok(contents) => println!("{contents}"),
+        Err(error) => {
+            eprintln!("There was an error: {error:?}");
+        }
+    }
 }

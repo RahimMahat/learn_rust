75664132f64444d4b9bd0c69f382e117b900cf71
RahimMahat <rahimmahat07@gmail.com>
2025-07-24 10:12:33 +0530

    adding notes till reading a file

--- Advanced/16_error_handling/src/main.rs
+++ Advanced/16_error_handling/src/main.rs
@@ -1,18 +1,35 @@
 use std::fs::File;
+use std::io::{Read, stdin};
 use std::process;
 
 fn file_handling() {
     // Opening a file.
-    let file = match File::open("notes.txt") {
+    println!("Enter filename to open: ");
+    let mut input = String::new();
+    let user_requested_file = stdin().read_line(&mut input);
+
+    if let Err(err) = user_requested_file {
+        eprintln!("Something went wrong getting user input: Error: {err}");
+        process::exit(1)
+    }
+
+    let mut file = match File::open(input.trim()) {
         Ok(file) => file,
         Err(err) => {
             // Prints to the standard error, while println! prints to the standard output, with a new line.
-            eprintln!("Something went wrong, error: {err:?}");
+            eprintln!("Something went wrong opening the file, error: {err}");
             // exit the program gracefully with an error code without panicking.
             process::exit(1);
         }
     };
-    println!("{file:#?}");
+
+    let mut file_contents = String::new();
+    let read_operation = file.read_to_string(&mut file_contents);
+    if let Err(err) = read_operation {
+        eprintln!("Something went wrong reading the file: error: {err}");
+        process::exit(1)
+    }
+    println!("{file_contents}");
 }
 
 fn main() {

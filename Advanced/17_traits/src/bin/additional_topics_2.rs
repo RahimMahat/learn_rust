540caadca61612ede85161a401fe1ab917126f13
RahimMahat <rahimmahat07@gmail.com>
2025-08-02 11:28:33 +0530

    implementing display trait on an enum

--- Advanced/17_traits/src/bin/additional_topics_2.rs
+++ Advanced/17_traits/src/bin/additional_topics_2.rs
@@ -1,7 +1,24 @@
 use std::fmt::{Display, Formatter, Result};
 
+enum AppleType {
+    RedDelicious,
+    GreenApples,
+}
+
+// Implementing Display trait to our custom enum.
+impl Display for AppleType {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> Result {
+        // Representing each enum variant distinctly you can just provide one uniform output to
+        // each variant, but just so we cover each differently we're doing this.
+        match self {
+            AppleType::RedDelicious => write!(formatter, "Red Apple"),
+            AppleType::GreenApples => write!(formatter, "Green Apple"),
+        }
+    }
+}
+
 struct Apple {
-    kind: String,
+    kind: AppleType,
     price: f64,
 }
 
@@ -10,16 +27,15 @@ impl Display for Apple {
     fn fmt(&self, formatter: &mut Formatter<'_>) -> Result {
         // we're passing the desired string structure ("{} apple for {}") to the fmt::Formatter
         // struct in the write! macro which will return Result of our struct in that format.
-        write!(formatter, "{} apple for {}", self.kind, self.price)
+        write!(formatter, "{} for {}", self.kind, self.price)
     }
 }
 
 fn main() {
     let lunch_snack = Apple {
-        kind: String::from("Regular"),
+        kind: AppleType::RedDelicious,
         price: 12.34,
     };
 
     println!("{}", lunch_snack);
 }
-

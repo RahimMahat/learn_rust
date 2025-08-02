81f274e7bd0d18a22e9cb79ed7907cd0070d12a6
RahimMahat <rahimmahat07@gmail.com>
2025-08-02 14:01:57 +0530

    implementing Copy trait

--- Advanced/17_traits/src/bin/additional_topics_3.rs
+++ Advanced/17_traits/src/bin/additional_topics_3.rs
@@ -3,6 +3,7 @@ use std::clone::Clone;
 // you can give a compiler directive on a struct to derive Clone trait just like the Debug trait.
 // this wll only work if all the field types in the struct implement clone trait
 // #[derive(Clone)]
+#[derive(Debug)]
 struct Appointment {
     doctor: String,
     start_time: String,
@@ -32,6 +33,32 @@ impl Clone for Appointment {
     }
 }
 
+// The Copy trait is the subtrait of Clone trait. In other words Clone trait is the supertrait of the Copy trait.
+// so if a type chooses implement Copy trait it has to implement Clone trait. But if a type chooses
+// to implement Clone trait it does not necessarily need to implement Copy trait.
+// instead of doing an impl block below to implement Copy trait you can use compiler directive here
+// as well like this:  #[derive(Clone, Copy)]
+#[derive(Debug, Clone)]
+struct Duration {
+    hours: u32,
+    minutes: u32,
+    seconds: u32,
+}
+
+impl Duration {
+    fn new(hours: u32, minutes: u32, seconds: u32) -> Self {
+        Self {
+            hours,
+            minutes,
+            seconds,
+        }
+    }
+}
+
+// Copy trait does not mandate any methods so you don't have to define any methods within the {}
+// The only condition being here is that the field type must implement Copy trait which in our case is u32 and implements Copy trait.
+impl Copy for Duration {}
+
 fn main() {
     let morning_appointment = Appointment::new("Dr. House", "9:00AM", "10:00AM");
     let replacement_appointment = morning_appointment.clone();
@@ -41,4 +68,11 @@ fn main() {
         replacement_appointment.start_time,
         replacement_appointment.end_time
     );
+    println!("{morning_appointment:?}"); // since we had implemented the Clone trait on the struct there was no move.
+
+    let one_hour = Duration::new(1, 0, 0);
+    let another_hour = one_hour;
+    println!("{:?}", another_hour);
+    println!("{:?}", one_hour); // since Duration struct is implementing Copy trait you can access
+    // the one_hour struct instance even after it has been reassigned to a different var.
 }

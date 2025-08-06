b8f955d73726880da428171d56d3166fd3dfe597
RahimMahat <rahimmahat07@gmail.com>
2025-08-06 10:36:13 +0530

    intro to generic lifetimes

--- Advanced/18_lifetimes/src/main.rs
+++ Advanced/18_lifetimes/src/main.rs
@@ -1,3 +1,22 @@
+// A function can not return a reference to the any owned value or parameter.
+// fn create(number: i32) -> &i32 {
+//     &number
+// }
+
+// the only way you can return a reference from a function is if it's coming from the parameter type.
+// i.e. if the parameter type & return type both are same.
+fn create(valid: &i32) -> &i32 {
+    &valid
+}
+
+// generic lifetime annotation
+// 'a is the lifetime annotation syntax. which in below example bounds the return reference
+// lifetime with the lifetime of it's referent items's lifetime i.e the returned slice can not
+// outlive the items collection
+fn generic_lifetime<'a>(items: &'a [String]) -> &'a [String] {
+    &items[..2]
+}
+
 fn main() {
     let cities = vec![
         String::from("Riyadh"),
@@ -5,7 +24,17 @@ fn main() {
         String::from("Doha"),
     ];
 
-    let favourite_cities = &cities[..2];
-    let places = cities;
+    let _favourite_cities = &cities[..2];
+    let _places = cities;
     // println!("{favourite_cities:?}"); // dangling reference.
+    // create(10);
+    create(&3);
+
+    let items = vec![
+        "a".to_string(),
+        "b".to_string(),
+        "c".to_string(),
+        "d".to_string(),
+    ];
+    generic_lifetime(&items);
 }

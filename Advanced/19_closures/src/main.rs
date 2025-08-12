efd135b1a6540fc8243c62b576db8530e110c781
RahimMahat <rahimmahat07@gmail.com>
2025-08-12 10:28:03 +0530

    the unwrap or else method

--- Advanced/19_closures/src/main.rs
+++ Advanced/19_closures/src/main.rs
@@ -20,4 +20,40 @@ fn main() {
     let print_numbers = || println!("{:?}", numbers); // Fn captures an immutable reference so no move occured.
     print_numbers();
     println!("{:?}", numbers);
+
+    // closure that captures a mutable reference.
+    let mut nums = vec![6, 7, 8];
+    let mut add_num = || nums.push(9); // FnMut: A borrow of mutable reference occured.
+    add_num();
+    add_num();
+    println!("{:?}", nums);
+
+    // closure with ownership.
+    let first_name = String::from("John"); // heap allocated data type
+    let capture_string = || first_name; // FnOnce: captures a value by ownership. i.e the value will be moved.
+    // A closure with FnOnce can not be called more than once.
+    // println!("{first_name}"); // can not access first_name here since move occured in the above line.
+    let _owner = capture_string;
+
+    // The 'move' keyword.
+    let last_name = String::from("Wick");
+    let capture_str = move || {
+        // initally we were not taking ownership of the
+        // last_name variable but the 'move' keyword override that behaviour and moved the value.
+        println!("{}", last_name);
+    };
+    // println!("{}", last_name); // due to using 'move' keyword above this will now throw error.
+    // Even though the value moved the capture_str is still implementing Fn trait so you can call
+    // this closure multiple times.
+    capture_str();
+    capture_str(); // this will work.
+
+    // The unwrap_or_else method.
+    let option = Some("Shawarma");
+    let food = option.unwrap_or_else(|| "Kebab"); // unwrap_or_else method expects to
+    // pass a FnOnce closure in the paranthesis.
+    println!("{food}");
+    let option: Option<&str> = None;
+    let food = option.unwrap_or_else(|| "Kebab");
+    println!("{food}");
 }

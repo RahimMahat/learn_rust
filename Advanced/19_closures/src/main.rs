056545e69035b12be5014a6c11f4aa4be5e42a6d
RahimMahat <rahimmahat07@gmail.com>
2025-08-14 11:19:16 +0530

    complete closure notes

--- Advanced/19_closures/src/main.rs
+++ Advanced/19_closures/src/main.rs
@@ -21,6 +21,32 @@ impl Vault {
     }
 }
 
+#[derive(Debug)]
+struct Location {
+    name: String,
+    treasure: u32,
+}
+
+#[derive(Debug)]
+struct Map<'a> {
+    locations: &'a [Location],
+}
+
+impl<'a> Map<'a> {
+    fn explore<F>(&self, mut action: F)
+    where
+        F: FnMut(&Location),
+    {
+        let final_index = self.locations.len() - 1;
+        let mut current_index = 0;
+        while current_index <= final_index {
+            let current_location = &self.locations[current_index];
+            action(current_location);
+            current_index += 1;
+        }
+    }
+}
+
 fn main() {
     let multiplier = 5;
 
@@ -111,4 +137,26 @@ fn main() {
     game_console.retain(retain_closure);
     println!("{}", game_console);
     println!("{}", deleted_characters);
+
+    // FnMut closure as function parameter.
+    let locations = [
+        Location {
+            name: String::from("Enchanted Forest"),
+            treasure: 5,
+        },
+        Location {
+            name: String::from("Mystic Mountain"),
+            treasure: 10,
+        },
+    ];
+
+    let map = Map {
+        locations: &locations,
+    };
+    let mut total_treasure = 0;
+    map.explore(|location| {
+        total_treasure += location.treasure;
+    });
+
+    println!("Total treasures collected {total_treasure}");
 }
